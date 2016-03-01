ebb1346c8c98c6d57a289b8cfd438546e72cf0ba
CharlieKeil <Keil0009@algonquinlive.com>
2016-03-01 11:01:40 -0500

    Fixed small error in if section for C

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-var storyTime = 'Let’s make a story together!';
-
-alert(storyTime);
+var storyTime = alert('Let’s make a story together!');
 
 var colour = prompt('A colour (orange, indigo, etc.)');
 
@@ -10,16 +8,16 @@ var adjective = prompt('An adjective (beautiful, super, etc.)');
 
 var verbed = prompt('A past tense verb (ran, burped, etc.)');
 
-var list = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning');
+var choose = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning');
 
-if(list == 'a') {
-  document.write('After snacking on ' + adjective + 'treats, the ' + colour + 'bellied ' + creature + verbed + 'for hours.')
+if(choose == 'a') {
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
 }
 
-if(list == 'b') {
-  document.write('Ameilia ' + verbed + 'through the ' + adjective + colour + 'nebula escaping the space ' + creature + '.')
+if(choose == 'b') {
+  document.write('Ameilia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + '.');
 }
 
-if(list == 'c') {
-  document.write('Jackson chose his' + adjective + ', ' + colour + creature 'card and ' + verbed + 'it to the table knowing he won.')
+if(choose == 'c') {
+  document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
 }

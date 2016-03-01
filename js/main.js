d29dce63a7985d34bd489656efeadec63fa89cdd
CharlieKeil <Keil0009@algonquinlive.com>
2016-03-01 10:40:58 -0500

    Changed prompts in javascript.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
-alert('Let’s make a story together!');
+var storyTime = 'Let’s make a story together!';
 
-var colour = prompt('A colour (orange, indigo, etc.');
+alert(storyTime);
 
----
+var colour ='A colour (orange, indigo, etc.)';
 
-A creature (dragon, moth, etc.)
+prompt(colour);
 
----
+var creature ='A creature (dragon, moth, etc.)';
 
-An adjective (beautiful, super, etc.)
+prompt(creature);
 
----
+var adjective ='An adjective (beautiful, super, etc.)';
 
-A past tense verb (ran, burped, etc.)
+prompt(adjective);
 
----
+var past ='A past tense verb (ran, burped, etc.)';
 
-Which story would you like?
-a) Snacking
-b) Escaping
-c) Winning
+prompt(past);
+
+var list ='Which story would you like?';
+var snack = 'Snacking';
+var esc = 'Escaping';
+var win = 'Winning';
+var a = ' a) ' + snack;
+var b = ' b) ' + esc;
+var c = ' c) ' + win;
+prompt(list + a + b + c);
+
+document.write('After snacking on' +  adjective + 'treats, the' + colour + 'bellied' + creature + past + 'for hours. Ameilia ' + past + 'through the ' + adjective + colour + 'nebula escaping the space ' + creature + '.' + 'Jackson chose his ' + adjective + ',' + colour + creature + 'card and ' + verbed + 'it to the table knowing he won.');
+
+/*
 
 Enter the letter below:
-
----
 
 After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
 
 Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
 
 Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
+*/
